9f0e2b9e720138a4fd50f64a82774b539a858020
Your Name <you@example.com>
2026-05-29 16:22:44 +0800

    Release grid view template callbacks

--- AllLive.UWP/Controls/MyAdaptiveGridView.cs
+++ AllLive.UWP/Controls/MyAdaptiveGridView.cs
@@ -14,6 +14,10 @@ namespace AllLive.UWP.Controls
 {
     public class MyAdaptiveGridView : AdaptiveGridView
     {
+        public MyAdaptiveGridView()
+        {
+            Unloaded += MyAdaptiveGridView_Unloaded;
+        }
 
         private ICommand _LoadMoreCommand;
         public ICommand LoadMoreCommand
@@ -53,35 +57,64 @@ namespace AllLive.UWP.Controls
 
 
         ScrollViewer scrollViewer;
+        long dataLoadingCallbackToken;
         protected override void OnApplyTemplate()
         {
-            scrollViewer = GetTemplateChild("ScrollViewer") as ScrollViewer;
-            scrollViewer.ViewChanged += ScrollViewer_ViewChanged;
-            this.RegisterPropertyChangedCallback(DataLoadingProperty, new DependencyPropertyChangedCallback((obj, e) => {
-                if (!DataLoading)
-                {
-                    if (scrollViewer.ScrollableHeight == 0)
-                    {
-                        LoadMoreCommand?.Execute(null);
-                    }
-                }
-            }));
-
+            DetachTemplateCallbacks();
             base.OnApplyTemplate();
+            scrollViewer = GetTemplateChild("ScrollViewer") as ScrollViewer;
+            if (scrollViewer != null)
+            {
+                scrollViewer.ViewChanged += ScrollViewer_ViewChanged;
+            }
+            dataLoadingCallbackToken = RegisterPropertyChangedCallback(DataLoadingProperty, OnDataLoadingChanged);
+        }
+
+        private void MyAdaptiveGridView_Unloaded(object sender, RoutedEventArgs e)
+        {
+            DetachTemplateCallbacks();
+        }
+
+        private void DetachTemplateCallbacks()
+        {
+            if (scrollViewer != null)
+            {
+                scrollViewer.ViewChanged -= ScrollViewer_ViewChanged;
+                scrollViewer = null;
+            }
+            if (dataLoadingCallbackToken != 0)
+            {
+                UnregisterPropertyChangedCallback(DataLoadingProperty, dataLoadingCallbackToken);
+                dataLoadingCallbackToken = 0;
+            }
+        }
+
+        private void OnDataLoadingChanged(DependencyObject obj, DependencyProperty property)
+        {
+            TryLoadMoreWhenContentDoesNotFill();
+        }
+
+        private void TryLoadMoreWhenContentDoesNotFill()
+        {
+            if (!DataLoading && scrollViewer != null && scrollViewer.ScrollableHeight == 0)
+            {
+                LoadMoreCommand?.Execute(null);
+            }
         }
 
         private void MyAdaptiveGridView_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
 
             Debug.WriteLine("内容变更");
-            if (scrollViewer.ScrollableHeight == 0)
-            {
-                LoadMoreCommand?.Execute(null);
-            }
+            TryLoadMoreWhenContentDoesNotFill();
         }
 
         private void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
         {
+            if (scrollViewer == null)
+            {
+                return;
+            }
             if (scrollViewer.VerticalOffset >= scrollViewer.ScrollableHeight - LoadMoreBottomOffset && CanLoadMore)
             {
                 LoadMoreCommand?.Execute(null);
